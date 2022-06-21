59f53950c1b671251f56c022fb2800debb7379da
tpate0562 <tpate0562@stu.wusd.org>
2022-06-21 14:46:16 -0700

    Initial Commit

---
+++ integrator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  integrator
+//
+//  Created by Tejas Patel on 6/21/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
